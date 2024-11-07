e58874e6c7d8d66b813a5fd67624d2a4bc0df72c
Santosh Choudhary <santosh@bikoapp.com>
2024-11-06 21:52:20 -0500

    updated

---
+++ build-tools/docker_dradlew_local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the current working directory (local project)
+PROJECT_DIR=$(pwd)
+
+# Ensure the gradle wrapper script is executable
+chmod +x gradlew
+
+# Run Gradle build using Docker for the environment, but run the build locally
+docker run --rm \
+    -v "$PROJECT_DIR:/app" \  # Mount the project directory to the container (read-only)
+    -w /app \  # Set working directory inside the container
+    your-docker-image gradle build  # Run gradle build (local execution)

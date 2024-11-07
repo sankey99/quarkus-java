3eb342d3c7c23cbf1e1c5878bb89f9409f4b5f9c
Santosh Choudhary <santosh@bikoapp.com>
2024-11-06 23:58:00 -0500

    updated

--- build-tools/setup.sh
+++ build-tools/setup.sh
@@ -1,10 +1,25 @@
 # Pull the Gradle image (if you don't already have it)
 docker pull gradle:8.3-jdk17
 
-# Create a temporary container, extract Gradle, and copy it to the host machine
-docker run --rm -v "$PWD":/mnt gradle:8.3-jdk17 bash -c "cp -r /opt/gradle /mnt/gradle"
+#!/bin/bash
 
-export GRADLE_HOME=$PWD/gradle/gradle-8.3
+# Get the home directory in an OS-independent way
+if [ -n "$HOME" ]; then
+  HOMEDIR="$HOME"
+elif [ -n "$USERPROFILE" ]; then
+  HOMEDIR="$USERPROFILE"
+else
+  echo "Unable to determine home directory"
+  exit 1
+fi
+
+echo "Home directory: $HOMEDIR"
+
+
+# Create a temporary container, extract Gradle, and copy it to the host machine
+docker run --rm -v "$HOMEDIR":/mnt gradle:8.3-jdk17 bash -c "cp -r /opt/gradle /mnt/gradle"
+
+export GRADLE_HOME=$HOMEDIR/gradle/gradle-8.3
 export PATH=$GRADLE_HOME/bin:$PATH
 source ~/.bashrc  # or ~/.zshrc, depending on your shell
 gradle --version
